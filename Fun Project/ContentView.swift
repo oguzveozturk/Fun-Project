18db7f73ed7430ca5bf64563e0ea6c1c7393da69
Oguz <oguzveozturk@gmail.com>
2021-12-10 20:06:00 +0300

    Initial Commit

---
+++ Fun Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fun Project
+//
+//  Created by Oğuz Öztürk on 10.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fun Project/Fun_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_ProjectApp.swift
+//  Fun Project
+//
+//  Created by Oğuz Öztürk on 10.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Fun_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
